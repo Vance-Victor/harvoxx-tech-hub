de5053d121da11a7a64c3ef9d2c31d3c3d4f2756
Vance-Victor <viktohvance@gmail.com>
2024-04-23 19:26:59 +0100

    SIGN UP PAGE

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,10 +4,34 @@ import Nav from './Nav'
 export default function Login(){
     return(
         <section>
-            <div>
-                <h2>LOG in</h2>
-            </div>
-            <div></div>
-        </section>
+        <div></div>
+        <div>
+            <form id="form">
+                <h3>Login</h3>
+        
+
+                <p id="erroremail" style={{ color: "red" }}></p>
+                <p>Email</p>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Your Email"
+                    className="yourEmail"
+                />
+
+
+                <p id="errorpassword" style={{ color: "red" }}></p>
+                <p>Password</p>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    className="yourPassword"
+                />
+                <input type="button" value="Login" />
+                <p>Dont have an Account? Create an account</p>
+            </form>
+        </div>
+    </section>
     )
 }
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,28 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Nav from './Nav'
+import welcomeImage from '../Images/welcome.png'
 
 export default function Signup(){
     return(
-        <section>
-        <div></div>
-        <div>
+        <section className="signup">
+        <div
+          className="image"
+        //   data-aos="fade-in"
+        //   data-aos-easing="linear"
+        //   data-aos-duration="1500"
+        >
+          <img src={welcomeImage} alt="welcome" />
+        </div>
+        <div className="myForm">
             <form id="form">
+                <h2>Create an Account</h2>
                 <p id="errorname" style={{ color: "red" }}></p>
-                <p></p>
+                <p>Full Name</p>
                 <input
                     type="text"
                     id="fullName"
                     placeholder="Full Name"
                     className="yourName" />
 
-                <p id="errorStaffID" style={{ color: "red" }}></p>
-                <input
-                    type="text"
-                    id="staffID"
-                    placeholder="Staff ID"
-                    className="yourStaffID" />
 
                 <p id="erroremail" style={{ color: "red" }}></p>
+                <p>Email</p>
                 <input
                     type="email"
                     id="email"
@@ -30,20 +35,18 @@ export default function Signup(){
                     className="yourEmail"
                 />
 
-                <p id="errorDepartment" style={{ color: "red" }}></p>
-                <input
-                    type="text"
-                    id="department"
-                    placeholder="Department"
-                    className="yourDepartment" />
 
                 <p id="errorpassword" style={{ color: "red" }}></p>
+                <p>Password</p>
                 <input
                     type="password"
                     id="password"
                     placeholder="Password"
                     className="yourPassword"
                 />
+                <br/>
+                <input className="button" type="button" value="Create Account" />
+                <p>Already have an account? <Link to="/login">Login</Link></p>
             </form>
         </div>
     </section>
